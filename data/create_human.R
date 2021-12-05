25655fc4f4cb85a69218dc976c52d85b8d582f89
House <andrew.house@helsinki.fi>
2021-12-05 21:23:02 +0200

    new

--- data/create_human.R
+++ data/create_human.R
@@ -60,3 +60,78 @@ hd_gii <- inner_join(hd, gii, by = "Country", suffix = c(".hd", ".gii"))
 #Let us check that all the columns and dimensions exist
 colnames(hd_gii)
 dim(hd_gii)
+str(hd_gii)
+library(stringr)
+
+# look at the structure of the GNI column in 'human'
+str(hd_gii$GNI)
+
+
+# read the human data
+human <- read.table("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt", sep  =",", header = T)
+
+# look at the (column) names of human
+names(human)
+
+# look at the structure of human
+str(human)
+
+# print out summaries of the variables
+summary(human)
+
+# access the stringr package
+library(stringr)
+
+# look at the structure of the GNI column in 'human'
+str(human$GNI)
+
+# remove the commas from GNI and print out a numeric version of it
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+
+
+# columns to keep
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Life.Exp", "Edu.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+
+# select the 'keep' columns
+human <- select(human, one_of(keep))
+
+# print out a completeness indicator of the 'human' data
+complete.cases(human)
+
+# print out the data along with a completeness indicator as the last column
+data.frame(human[-1], comp = complete.cases(human))
+
+# filter out all rows with NA values
+human_ <- filter(human, complete.cases(human))
+
+
+
+# look at the last 10 observations
+tail(human, 10)
+
+# last indice we want to keep
+last <- nrow(human) - 7
+
+# choose everything until the last 7 observations
+human_ <- human[1:last, ]
+
+# add countries as rownames
+rownames(human) <- human$Country
+
+# remove the Country variable
+human_ <- select(human, -Country)
+
+library(stringr)
+library(dplyr)
+
+#Reading the data
+human = read.table(file="hd_gii.txt", sep=",", header=TRUE)
+
+data.frame(human[-1], comp = complete.cases(human))
+
+#Filtering out all rows with NA (missing) values
+human_na <- filter(human, complete.cases(human) == TRUE)
+human <- filter(human, complete.cases(human) == TRUE)
+human$GNI <- as.numeric(human$GNI)
+str(human)
